519d364156795c9a1554f7ba6d8538d4b3cfcb3d
Vadimk <vadimkotkin1855@gmail.com>
2024-06-16 21:46:27 +0200

    mac added to log-ins

--- middleware/recordLogIns.js
+++ middleware/recordLogIns.js
@@ -1,8 +1,14 @@
 const {format} = require('date-fns'); 
+const macaddress = require('macaddress'); 
 const recordLogIns = async(message, req, user) =>{ 
     const ip = req.headers['x-forwarded-for'];
     const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
-    const logItem = `${message} from ${ip} at ${dateTime}`;
+    const logItem = `${message} from Ip ${ip}, Mac ${macaddress.one((err, mac)=>{
+        if(err) {
+            console.error("Error while getting Mac", err); 
+            return;}
+        else return mac;
+    })} at ${dateTime}`;
     user.logIns.push(logItem);
     await user.save();
 };
