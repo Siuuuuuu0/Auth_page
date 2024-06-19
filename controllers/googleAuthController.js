5b1a10600c20e6d8443ceb1dab464416bcd2f6b8
Vadimk <vadimkotkin1855@gmail.com>
2024-06-19 22:59:04 +0200

    google oauth2

--- controllers/googleAuthController.js
+++ controllers/googleAuthController.js
@@ -5,7 +5,7 @@ const handleGoogleAuth = async(req, res)=>{
     passport.use(new GoogleStrategy({
         clientID : process.env.GOOGLE_CLIENT_ID, 
         clientSecret : process.env.CLIENT_SECRET,
-        callbackURL : "/auth/google/callback"
+        callbackURL : "/auth/google/callback" //if registration auth route to register username and password, else the root route
     }, 
     async(accessToken, refreshToken, profile, done)=>{
         try{
@@ -31,9 +31,8 @@ const handleGoogleAuth = async(req, res)=>{
                 foundUser.refreshToken = refresh_Token;
                 foundUser.lastLocation = req.body.location;
                 await foundUser.save();
-                // req.session.userId = foundUser._id;
                 recordLogIns("New log in from ", req, foundUser); //no need for sync work
-                res.cookie('jwt', refreshToken, {httpOnly : true, sameSite : "None", maxAge : 1000*60*60*24});
+                res.cookie('jwt', refresh_Token, {httpOnly : true, sameSite : "None", maxAge : 1000*60*60*24});
                 // res.cookie('jwt', refreshToken, {httpOnly : true, secure :true, sameSite : "None", maxAge : 1000*60*60*24});
                 res.json({access_Token});
                 return done(null, foundUser);
@@ -42,11 +41,12 @@ const handleGoogleAuth = async(req, res)=>{
                 const result = await User.create({
                     googleId : profile.id, 
                     email : profile.emails[0].value, 
-                    username : profile.emails[0].value, //temporary 
+                    username : '', //temporary 
                     password : '' //temporary
                 });
                 console.log(result);
-                return done(null, result);
+                const toRegister = true;
+                return done(null, {result, toRegister});
             }
         }
         catch(err){
