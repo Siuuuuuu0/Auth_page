3e77f1b9c3c750547cce93c589e016289cf4655d
Vadimk <vadimkotkin1855@gmail.com>
2024-06-25 16:32:44 +0200

    minor change

--- middleware/setAcessTokenCookie.js
+++ middleware/setAcessTokenCookie.js
@@ -5,8 +5,8 @@ const setAccessTokenCookie = (req, res, next) => {
       const accessToken = req.user.access_Token;
       // logEvents(JSON.stringify(accessToken), 'testing.txt');
       res.cookie('jwt', accessToken, { httpOnly: true, sameSite: "None", maxAge: 1000 * 60 * 60 * 24 });
-      logEvents(JSON.stringify(next), 'testingNext.txt');
-      return res.json({accessToken});
+      // logEvents(JSON.stringify(next), 'testingNext.txt');
+      return res.json({accessToken}); //redirect to main page
       // res.cookie('jwt', accessToken, { httpOnly: true, secure : true, sameSite: "None", maxAge: 1000 * 60 * 60 * 24 });
     }
     else return next();
